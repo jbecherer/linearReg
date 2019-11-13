bc0bf39280d10ef1a31c64c1db5b5ad6602fad44
jbecherer <jbecherer@gmx.net>
2019-11-13 15:05:41 -0800

    extended the linear regression script to do all kind of statistics

--- test_linearReg.m
+++ test_linearReg.m
@@ -5,20 +5,31 @@ clear all;
 close all;
 
 % create test signal
-N = 1000;
-xl = [0 3];
+N = 20;
+xl = [0 5];
 x = xl(2)*rand(N,1);
 y = x + (rand(N,1)-.5);
+N_noise = round(.2*length(y));
+%y(1:N_noise) = xl(2)*rand(N_noise,1);
+
 
 % standart polyfit
 tic
-p = polyfit(x,y,1)
+p = polyfit(x,y,1);
 toc
 
 % linear regression;
 tic
-[lg] = linearReg(x,y)
+[lg, std_lg, con_lg, r, con_r] = linearReg(x,y)
 toc
+
+% correlation
+tic
+[r_ref, ~, rl, ru] = corrcoef(x,y);
+toc
+
+con_r_ref = [rl(1,2) r_ref(1,2) ru(1,2)]
+
 
 figure
 plot(x,y,'.')
